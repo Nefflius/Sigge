ebf92100b3fad478a68465cd9927add3802f5180
Hampus Wallin <Neflit89@gmail.com>
2014-01-29 21:50:05 +0100

    Enhanced the comment to perfection in the grace of god and ogers!

--- Sudoku/Sudoku/MainWindow.xaml.cs
+++ Sudoku/Sudoku/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace Sudoku
 
         private void clickAvsluta(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();             //Stäng av programmet
+            Application.Current.Shutdown();             //Stäng av programmet, direkt!
         }
 
         private void clickRätta(object sender, RoutedEventArgs e)
